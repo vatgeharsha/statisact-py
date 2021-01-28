259d221a8b728bcf856de1485d31cb6fa6bccba7
vatgeharsha <51117677+vatgeharsha@users.noreply.github.com>
2021-01-28 21:25:35 +0530

    updated calculateStats

--- statistics.py
+++ statistics.py
@@ -1,3 +1,4 @@
-
-def calculateStats(numbers):
-  return None
+def calculateStats(val):
+    if len(val) == 0:
+        return float("nan")
+    return {"avg":sum(val)/len(val),"max":max(val),"min":min(val)}
